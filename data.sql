a8009d1ab6877f7be34af8d1f51f804524a01b48
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-08-14 20:56:06 +0530

    Animals-table🐶😸🐷🐭

---
+++ data.sql
@@ -0,0 +1,13 @@
+INSERT INTO
+   animals (
+      name,
+      date_of_birth,
+      escape_attempts,
+      neutered,
+      weight_kg
+   )
+VALUES
+   ('Agumon', '2020-02-03', 0, TRUE, 10.23),
+   ('Gabumon', '2018-11-15', 2, TRUE, 8),
+   ('Pikachu', '2021-01-07', 1, FALSE, 15.04),
+   ('Devimon', '2017-05-12', 5, TRUE, 11);
