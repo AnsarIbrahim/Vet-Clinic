0563d71b42dc82c7b7f4498fc4ae3d403e14f7cf
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-08-16 19:49:47 +0530

    Query multiple tables 🤹🤹🤹

--- data.sql
+++ data.sql
@@ -1,3 +1,4 @@
+-- Animals
 INSERT INTO
    animals (
       name,
@@ -28,3 +29,120 @@ VALUES
    ('Boarmon', '2005-06-07', 7, TRUE, 20.4),
    ('Blossom', '1998-10-13', 3, TRUE, 17),
    ('Ditto', '2022-05-14', 4, TRUE, 22);
+
+-- Update Animals
+UPDATE
+   animals
+SET
+   species_id = (
+      SELECT
+         id
+      FROM
+         species
+      WHERE
+         name = 'Digimon'
+   )
+WHERE
+   name LIKE '%mon';
+
+UPDATE
+   animals
+SET
+   species_id = (
+      SELECT
+         id
+      FROM
+         species
+      WHERE
+         name = 'Pokemon'
+   )
+WHERE
+   species_id IS NULL;
+
+UPDATE
+   animals
+SET
+   owner_id = (
+      SELECT
+         id
+      FROM
+         owners
+      WHERE
+         full_name = 'Sam Smith'
+   )
+WHERE
+   name = 'Agumon';
+
+UPDATE
+   animals
+SET
+   owner_id = (
+      SELECT
+         id
+      FROM
+         owners
+      WHERE
+         full_name = 'Jennifer Orwell'
+   )
+WHERE
+   name IN ('Gabumon', 'Pikachu');
+
+UPDATE
+   animals
+SET
+   owner_id = (
+      SELECT
+         id
+      FROM
+         owners
+      WHERE
+         full_name = 'Bob'
+   )
+WHERE
+   name IN ('Devimon', 'Plantmon');
+
+UPDATE
+   animals
+SET
+   owner_id = (
+      SELECT
+         id
+      FROM
+         owners
+      WHERE
+         full_name = 'Melody Pond'
+   )
+WHERE
+   name IN ('Charmander', 'Squirtle', 'Blossom');
+
+UPDATE
+   animals
+SET
+   owner_id = (
+      SELECT
+         id
+      FROM
+         owners
+      WHERE
+         full_name = 'Dean Winchester'
+   )
+WHERE
+   name IN ('Angemon', 'Boarmon');
+
+-- Owners
+INSERT INTO
+   owners (full_name, age)
+VALUES
+   ('Sam Smith', 34),
+   ('Jennifer Orwell', 19),
+   ('Bob', 45),
+   ('Melody Pond', 77),
+   ('Dean Winchester', 14),
+   ('Jodie Whittaker', 38);
+
+-- Species
+INSERT INTO
+   species (name)
+VALUES
+   ('Pokemon'),
+   ('Digimon');
