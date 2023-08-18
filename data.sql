b73e17c00aa15eaeef083f00b3407b25f859a012
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-08-18 13:19:30 +0530

    Add-join-table-visits👀👀👀

--- data.sql
+++ data.sql
@@ -146,3 +146,474 @@ INSERT INTO
 VALUES
    ('Pokemon'),
    ('Digimon');
+
+-- vets
+INSERT INTO
+   vets (name, age, date_of_graduation)
+VALUES
+   ('William Tatcher', 45, '2000-04-23'),
+   ('Maisy Smith', 26, '2019-01-17'),
+   ('Stephanie Mendez', 64, '1981-05-04'),
+   ('Jack Harkness', 38, '2008-06-08');
+
+-- Specializations
+INSERT INTO
+   specializations (vet_id, species_id)
+VALUES
+   (
+      (
+         SELECT
+            id
+         FROM
+            vets
+         WHERE
+            name = 'William Tatcher'
+      ),
+      (
+         SELECT
+            id
+         FROM
+            species
+         WHERE
+            name = 'Pokemon'
+      )
+   ),
+   (
+      (
+         SELECT
+            id
+         FROM
+            vets
+         WHERE
+            name = 'Stephanie Mendez'
+      ),
+      (
+         SELECT
+            id
+         FROM
+            species
+         WHERE
+            name = 'Digimon'
+      )
+   ),
+   (
+      (
+         SELECT
+            id
+         FROM
+            vets
+         WHERE
+            name = 'Stephanie Mendez'
+      ),
+      (
+         SELECT
+            id
+         FROM
+            species
+         WHERE
+            name = 'Pokemon'
+      )
+   ),
+   (
+      (
+         SELECT
+            id
+         FROM
+            vets
+         WHERE
+            name = 'Jack Harkness'
+      ),
+      (
+         SELECT
+            id
+         FROM
+            species
+         WHERE
+            name = 'Digimon'
+      )
+   );
+
+-- Visits
+INSERT INTO
+   visits (vet_id, animal_id, visit_date)
+VALUES
+   (
+      (
+         SELECT
+            id
+         FROM
+            vets
+         WHERE
+            name = 'William Tatcher'
+      ),
+      (
+         SELECT
+            id
+         FROM
+            animals
+         WHERE
+            name = 'Agumon'
+      ),
+      '2020-05-24'
+   ),
+   (
+      (
+         SELECT
+            id
+         FROM
+            vets
+         WHERE
+            name = 'Stephanie Mendez'
+      ),
+      (
+         SELECT
+            id
+         FROM
+            animals
+         WHERE
+            name = 'Agumon'
+      ),
+      '2020-07-22'
+   ),
+   (
+      (
+         SELECT
+            id
+         FROM
+            vets
+         WHERE
+            name = 'Jack Harkness'
+      ),
+      (
+         SELECT
+            id
+         FROM
+            animals
+         WHERE
+            name = 'Gabumon'
+      ),
+      '2021-02-02'
+   ),
+   (
+      (
+         SELECT
+            id
+         FROM
+            vets
+         WHERE
+            name = 'Maisy Smith'
+      ),
+      (
+         SELECT
+            id
+         FROM
+            animals
+         WHERE
+            name = 'Pikachu'
+      ),
+      '2020-01-05'
+   ),
+   (
+      (
+         SELECT
+            id
+         FROM
+            vets
+         WHERE
+            name = 'Maisy Smith'
+      ),
+      (
+         SELECT
+            id
+         FROM
+            animals
+         WHERE
+            name = 'Pikachu'
+      ),
+      '2020-03-08'
+   ),
+   (
+      (
+         SELECT
+            id
+         FROM
+            vets
+         WHERE
+            name = 'Maisy Smith'
+      ),
+      (
+         SELECT
+            id
+         FROM
+            animals
+         WHERE
+            name = 'Pikachu'
+      ),
+      '2020-05-14'
+   ),
+   (
+      (
+         SELECT
+            id
+         FROM
+            vets
+         WHERE
+            name = 'Stephanie Mendez'
+      ),
+      (
+         SELECT
+            id
+         FROM
+            animals
+         WHERE
+            name = 'Devimon'
+      ),
+      '2021-05-04'
+   ),
+   (
+      (
+         SELECT
+            id
+         FROM
+            vets
+         WHERE
+            name = 'Jack Harkness'
+      ),
+      (
+         SELECT
+            id
+         FROM
+            animals
+         WHERE
+            name = 'Charmander'
+      ),
+      '2021-02-24'
+   ),
+   (
+      (
+         SELECT
+            id
+         FROM
+            vets
+         WHERE
+            name = 'Maisy Smith'
+      ),
+      (
+         SELECT
+            id
+         FROM
+            animals
+         WHERE
+            name = 'Plantmon'
+      ),
+      '2019-12-21'
+   ),
+   (
+      (
+         SELECT
+            id
+         FROM
+            vets
+         WHERE
+            name = 'William Tatcher'
+      ),
+      (
+         SELECT
+            id
+         FROM
+            animals
+         WHERE
+            name = 'Plantmon'
+      ),
+      '2020-08-10'
+   ),
+   (
+      (
+         SELECT
+            id
+         FROM
+            vets
+         WHERE
+            name = 'Maisy Smith'
+      ),
+      (
+         SELECT
+            id
+         FROM
+            animals
+         WHERE
+            name = 'Plantmon'
+      ),
+      '2021-04-07'
+   ),
+   (
+      (
+         SELECT
+            id
+         FROM
+            vets
+         WHERE
+            name = 'Stephanie Mendez'
+      ),
+      (
+         SELECT
+            id
+         FROM
+            animals
+         WHERE
+            name = 'Squirtle'
+      ),
+      '2019-09-29'
+   ),
+   (
+      (
+         SELECT
+            id
+         FROM
+            vets
+         WHERE
+            name = 'Jack Harkness'
+      ),
+      (
+         SELECT
+            id
+         FROM
+            animals
+         WHERE
+            name = 'Angemon'
+      ),
+      '2020-10-03'
+   ),
+   (
+      (
+         SELECT
+            id
+         FROM
+            vets
+         WHERE
+            name = 'Jack Harkness'
+      ),
+      (
+         SELECT
+            id
+         FROM
+            animals
+         WHERE
+            name = 'Angemon'
+      ),
+      '2020-11-04'
+   ),
+   (
+      (
+         SELECT
+            id
+         FROM
+            vets
+         WHERE
+            name = 'Maisy Smith'
+      ),
+      (
+         SELECT
+            id
+         FROM
+            animals
+         WHERE
+            name = 'Boarmon'
+      ),
+      '2019-01-24'
+   ),
+   (
+      (
+         SELECT
+            id
+         FROM
+            vets
+         WHERE
+            name = 'Maisy Smith'
+      ),
+      (
+         SELECT
+            id
+         FROM
+            animals
+         WHERE
+            name = 'Boarmon'
+      ),
+      '2019-05-15'
+   ),
+   (
+      (
+         SELECT
+            id
+         FROM
+            vets
+         WHERE
+            name = 'Maisy Smith'
+      ),
+      (
+         SELECT
+            id
+         FROM
+            animals
+         WHERE
+            name = 'Boarmon'
+      ),
+      '2020-02-27'
+   ),
+   (
+      (
+         SELECT
+            id
+         FROM
+            vets
+         WHERE
+            name = 'Maisy Smith'
+      ),
+      (
+         SELECT
+            id
+         FROM
+            animals
+         WHERE
+            name = 'Boarmon'
+      ),
+      '2020-08-03'
+   ),
+   (
+      (
+         SELECT
+            id
+         FROM
+            vets
+         WHERE
+            name = 'Stephanie Mendez'
+      ),
+      (
+         SELECT
+            id
+         FROM
+            animals
+         WHERE
+            name = 'Blossom'
+      ),
+      '2020-05-24'
+   ),
+   (
+      (
+         SELECT
+            id
+         FROM
+            vets
+         WHERE
+            name = 'William Tatcher'
+      ),
+      (
+         SELECT
+            id
+         FROM
+            animals
+         WHERE
+            name = 'Blossom'
+      ),
+      '2021-01-11'
+   );
--- schema.sql
+++ schema.sql
@@ -42,3 +42,26 @@ CREATE TABLE species (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255)
 );
+
+-- Table for vets
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    age INT,
+    date_of_graduation DATE
+);
+
+-- Table for Specializations
+CREATE TABLE specializations (
+    id SERIAL PRIMARY KEY,
+    vet_id INT REFERENCES vets(id),
+    species_id INT REFERENCES species(id)
+);
+
+-- Table for Visits
+CREATE TABLE visits (
+    id SERIAL PRIMARY KEY,
+    vet_id INT REFERENCES vets(id),
+    animal_id INT REFERENCES animals(id),
+    visit_date DATE
+);
